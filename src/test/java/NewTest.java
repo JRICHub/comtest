bf6e1e96571b80f18eb6405c449be9b37a76d4b2
Vague <786091903@qq.com>
2018-03-07 21:02:17 +0800

    update encoding utf-8 2

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,25 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-//@Listeners({com.hcb.test.core.listener.PowerEmailableReporter.class,com.hcb.test.core.listener.TestResultListener.class,com.hcb.test.core.listener.RetryListener.class})
 public class NewTest {
 
     private WebDriver driver;
     protected DesiredCapabilities capabilities;
     @BeforeTest
     public void beforeTest() throws Exception {
-//        driver = new FirefoxDriver();
-//        driver = new ChromeDriver();
-
         capabilities = DesiredCapabilities.chrome();
         capabilities.setBrowserName("chrome");
         System.setProperty("webdriver.chrome.driver", getClass().getResource("/chromedriver.exe").getPath());
@@ -31,16 +25,15 @@ public class NewTest {
         options.addArguments("--start-maximized");
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         driver = new ChromeDriver(capabilities);
-//        driver = new RemoteWebDriver(new URL("http://172.16.90.217:4444/wd/hub"), capabilities);
         driver.manage().window().maximize();
 
-        driver.get("http://www.baidu.com");
     }
 
     @AfterTest
     public void afterTest(){
         try {
-            Thread.sleep(10000);
+            //等待2秒查看执行效果
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -49,24 +42,22 @@ public class NewTest {
 
     @Test
     public void testClass() {
+        driver.get("http://www.baidu.com");
         System.out.println("heloo");
         By inputBox = By.id("kw");
         By searchButton = By.id("su");
-
-        
         //智能等待元素加载出来
         intelligentWait(driver, 10, inputBox);
         //智能等待元素加载出来
         intelligentWait(driver, 10, searchButton);
-        driver.findElement(inputBox).sendKeys("中国");
+        driver.findElement(inputBox).sendKeys("中国地图");
         driver.findElement(searchButton).click();
     }
-
 
     /**这是智能等待元素加载的方法*/
     public void intelligentWait(WebDriver driver,int timeOut, final By by) {
         try {
-            (new WebDriverWait(driver, timeOut)).until(new ExpectedCondition<Boolean>() {
+            (new WebDriverWait(driver, timeOut)).until(new ExpectedCondition<Boolean>(){
                 public Boolean apply(WebDriver driver) {
                     WebElement element = driver.findElement(by);
                     return element.isDisplayed();
